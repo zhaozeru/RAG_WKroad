9b53697ae38e4b768f7d227fc90fb01e62c5d431
zzr <147326294+zhaozeru@users.noreply.github.com>
2024-08-22 10:33:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import streamlit as st
-from langchain.memory import ConversationBufferMemory
-from utils import qa_agent
-import openai
+from utils import model_output
+
+# 陈果夫和陈立夫什么关系？
+knowledge_id_list = {
+    "陈立夫旧居": "1826433526632005632",
+    "颜福庆旧居": "1826433526590062592",
+    "武康庭(FERGUSON LANE）": "1826433526426492928",
+    "王元化故居": "1826433526317441024",
+    "陈果夫旧居": "1826433526304858112",
+    "莫觞清旧居": "1826433526204186624",
+    "黄兴故居": "1826433526199877632",
+    "马步芳故居": "1826433525998551040",
+    "巴金故居": "1826433493404729344",
+    "宋庆龄故居": "1826433493400420352",
+    "唐绍仪旧居": "1826433493287292928",
+    "张乐平故居": "1826433493203410944",
+    "柯灵故居": "1826433493178236928",
+    "周作民旧居": "1826433493094350848"
+}
 
 st.markdown(
     """
@@ -18,35 +34,38 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title("💬 LangChain RAG PDF问答与对话工具！")
+st.title("💬 RAG(Retrieval-Augmented Generation)")
+st.markdown("## &nbsp;👉 问答与对话工具！")
 st.divider()
 
-
 if st.button("🔄 开始新对话"):
-    st.session_state['user_questions'] = []
-    st.session_state['memory'] = ConversationBufferMemory(return_messages=True)
-    st.session_state['messages'] = [
-        {"role": "ai", "content": "你好！欢迎回来，我们可以重新开始对话。"}
-    ]
+    try:
+        st.session_state['user_questions'] = []
+        st.session_state['message'] = []
+        st.empty()
+        st.session_state['message'] = [
+            {"role": "assistant", "content": "您好！欢迎回来，我们可以重新开始对话。"}
+        ]
+    except Exception as e:
+        st.info(f"⚠️ 发生错误：{str(e)}")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入您的OpenAI API密钥：", type="password")
-    st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
-
     st.markdown("## 💡 功能说明")
     st.markdown(
-        "通过输入问题，您可以向PDF文档提问，AI助手会基于您的PDF文档内容进行回答。  \n"
-        "若回答有误请见谅。ChatGPT也可能会犯错，请核查重要信息！！🚨"
+        "通过输入问题，您可以向知识库文档提问，AI助手会基于文档内容进行回答。  \n  \n"
+        "您可以选择对整个故居知识库进行问答，也可以返回上一级选择某个故居进行针对性问答！  \n  \n"
+        "若回答有误请见谅。大模型也可能会犯错，请核查重要信息！！🚨"
     )
 
-
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
 
     st.markdown("## 🎛️ 模型参数调节")
     temperature = st.slider("温度 (Temperature)", 0.0, 1.0, 0.1, 0.1)
-    max_tokens = st.slider("最大生成长度 (Max Tokens)", 50, 500, 500, 10)
-    top_p = st.slider("顶部_p (Top-p)", 0.0, 1.0, 0.9, 0.1)
-    frequency_penalty = st.slider("频率惩罚 (Frequency Penalty)", 0.0, 2.0, 0.3, 0.1)
-    presence_penalty = st.slider("存在惩罚 (Presence Penalty)", 0.0, 2.0, 0.3, 0.1)
+    max_tokens = st.slider("最大生成长度 (Max Tokens)", 50, 500, 400, 10)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
 
     with st.expander("📜 历史问题", expanded=False):
         if st.session_state.get("user_questions"):
@@ -55,66 +74,49 @@ with st.sidebar:
         else:
             st.markdown("暂无历史问题。")
 
-name = st.query_params.get("name", "空")
-if name == "":
-    name = "空"
+try:
+    name = st.query_params.get("name", "空")
+    if name == "":
+        name = "故居知识库"
 
+    if name != "空":
+        knowledge_id = knowledge_id_list.get(name)
+    elif name == "故居知识库":
+        knowledge_id = '1826238715882885120'
 
-if "memory" not in st.session_state:
-    st.session_state["memory"] = ConversationBufferMemory(
-        return_messages=True,
-        memory_key="chat_history",
-        output_key="answer"
-    )
-    st.session_state["messages"] = [
-        {"role": "ai", "content": f"您好，我是您的AI助手，您的咨询对象为{name}，请问有什么可以帮你的吗？"}
-    ]
+    if "message" not in st.session_state:
+        st.session_state["message"] = [{
+            "role": "assistant", "content": f"您好，我是您的AI助手，您的咨询对象为{name}，请问有什么可以帮你的吗？"
+        }]
+    if "user_questions" not in st.session_state:
+        st.session_state["user_questions"] = []
 
-if "user_questions" not in st.session_state:
-    st.session_state["user_questions"] = []
+    for message in st.session_state["message"]:
+        st.chat_message(message["role"]).write(message["content"])
 
-for message in st.session_state["messages"]:
-    st.chat_message(message["role"]).write(message["content"])
+except KeyError as ke:
+    st.info(f"⚠️ 配置错误：{str(ke)}")
+except Exception as e:
+    st.info(f"⚠️ 发生错误：{str(e)}")
 
-prompt = st.chat_input("✨ 请输入您咨询的问题:")
+question = st.chat_input("✨ 请输入您咨询的问题:")
 
+if question:
+    try:
+        st.session_state["user_questions"].append(question)
+        st.session_state["message"].append({"role": "user", "content": question})
+        st.chat_message("human").write(question)
 
+        with st.spinner("💦 AI正在思考中，请稍等..."):
+            response = model_output(
+                knowledge_id=knowledge_id,
+                messages=st.session_state["message"],
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
 
-
-
-if prompt and not openai_api_key:
-    st.info("请输入你的OpenAI API密钥")
-    st.stop()
-
-if prompt and openai_api_key:
-    if name == "空":
-        st.info("请返回上一级，选择一个故居作为咨询对象进行问答。")
-        st.stop()
-
-    st.session_state["messages"].append({"role": "human", "content": prompt})
-    st.chat_message("human").write(prompt)
-    st.session_state["user_questions"].append(prompt)
-
-    with st.spinner("AI正在思考中，请稍等..."):
-        response = qa_agent(
-            openai_api_key,
-            st.session_state["memory"],
-            name,
-            prompt,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            top_p=top_p,
-            frequency_penalty=frequency_penalty,
-            presence_penalty=presence_penalty
-        )
-
-
-    st.session_state["messages"].append({"role": "ai", "content": response["answer"]})
-    st.chat_message("ai").write(response["answer"])
-
-
-    with st.expander("📄 查看源文档", expanded=False):
-        if response["source_documents"]:
-            st.write(response["source_documents"])
-        else:
-            st.write("暂无源文档信息。")
+        st.session_state["message"].append(response)
+        st.chat_message("assistant").write(response["content"])
+    except Exception as e:
+        st.info(f"⚠️ 发生错误：{str(e)}")
+        st.markdown("[获取错误码文档](https://open.bigmodel.cn/dev/api#queryfile)")
--- utils.py
+++ utils.py
@@ -1,90 +1,57 @@
-from langchain.chains import ConversationalRetrievalChain
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-import os
-import streamlit as st
-from langchain.prompts import PromptTemplate
+from zhipuai import ZhipuAI
 
-
-def qa_agent(openai_api_key,memory, name, question, temperature, max_tokens, top_p, frequency_penalty, presence_penalty):
-    pdf_path = f"{name}.pdf"
-
-    if not os.path.exists(pdf_path):
-        return {"answer": "找不到对应的文件。", "chat_history": memory}
-
-    if "db" not in st.session_state:
-        loader = PyPDFLoader(pdf_path)
-        docs = loader.load()
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=500,
-            chunk_overlap=100,
-            separators=[",","\n","。","，",""]
-        )
-        texts = text_splitter.split_documents(docs)
-
-        embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key,openai_api_base="https://api.aigc369.com/v1")
-        db = FAISS.from_documents(texts, embeddings_model)
-        st.session_state["db"] = db
-
-
-    db = st.session_state["db"]
-    retriever = db.as_retriever(search_kwargs={"k": 2})
-
-
-    relevant_docs = retriever.get_relevant_documents(question)
-    context = " ".join([doc.page_content for doc in relevant_docs])
-    prompt_template = PromptTemplate(
-        template="""
+def model_output(knowledge_id, messages, temperature,max_tokens):
+    ZHIPU_API_KEY = "3f2fa69c13a5828f049de33d2d05cfcc.tFt8YLHcgUb8j7g4"
+    client = ZhipuAI(api_key=ZHIPU_API_KEY)
+    template = """
         你是一个基于给定文档并进行问答的AI助手。
         文档的内容是上海武康路上名人故居的相关数据。
-       
+
         文档内容：
-        {context};
+        {{knowledge}};
         用户提问：
-        {question}
+        {{question}};
 
         先对用户提问进行判断：
         1、如果是和文档内容相关的问题，那么就请你以一个当地导游的角色，根据提供的文档内容来回答用户的问题。
-        当然，若超出了文档内容你可以告知用户超出知识库范围无法作答或者你可以不依靠知识库进行作答；
         2、如果是用户的寒暄、交谈或者与文档无关，那么这就是一次聊天，你可以和用户温柔地交谈而无需借助文档内容。
-
-        """,
-        input_variables=["context", "question"]
-    )
-
-
-
-    final_prompt = prompt_template.format(context=context, question=question)
-
-    model = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        openai_api_key=openai_api_key,
-        openai_api_base="https://api.aigc369.com/v1",
+        
+        请注意！！
+        若超出了文档内容你必须告知用户超出知识库范围或者你可以在告知后不依靠知识库进行作答。
+        不要透漏你的判断逻辑，让你的回答像是一次交谈，而不是一次分析。
+        """
+    response = client.chat.completions.create(
+        model="glm-4-air",
+        messages=messages,
+        tools=[
+            {
+                "type": "retrieval",
+                "retrieval": {
+                    "knowledge_id": knowledge_id,
+                    "prompt_template": template
+                }
+            }
+        ],
+        stream=True,
         temperature=temperature,
-        max_tokens=max_tokens,
-        top_p=top_p,
-        frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty
+        max_tokens=max_tokens
     )
+    ai_resp = ''
+    deltas = []
+    for chunk in response:
+        if chunk.choices:
+            delta = chunk.choices[0].delta
+            if delta:
+                print(delta.content, end='', flush=True)
+                deltas.append(delta.content)
+    ai_resp = ''.join(deltas)
+    return {"role": "assistant", "content": ai_resp}
 
-
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=model,
-        retriever=retriever,
-        memory=memory,
-        return_source_documents=True
-    )
-
-    response = qa.invoke({"chat_history": memory, "question": final_prompt})
-
-    source_documents = response.get("source_documents", [])
-    source_text = "\n\n".join([doc.page_content for doc in source_documents[:1]])
-
-    return {
-        "answer": response["answer"],
-        "source_documents": source_text,
-        "chat_history": memory
-    }
+# messages=[
+#     {"role": "user", "content": '请问陈果夫和陈立夫什么关系？'}
+# ]
+# temperature=0.1
+# knowledge_id = '1826239520555913216'
+# max_tokens = 100
+# res = model_output(knowledge_id, messages, temperature,max_tokens)
+# print(res)
